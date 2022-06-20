936dd081451edda3d9ba1ff69dd9afffb920450a
jenscocquyt <cocquytjens@gmail.com>
2022-06-20 15:14:59 +0200

    app start

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,18 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import { TopMoviesComponent } from './top-movies/top-movies.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    TopMoviesComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    NgbModule
   ],
   providers: [],
   bootstrap: [AppComponent]
