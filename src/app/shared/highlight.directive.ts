5299acb69b498bd076cf157488c37ef9012e0a13
jenscocquyt <cocquytjens@gmail.com>
2022-06-25 15:03:39 +0200

    movie overview

---
+++ src/app/shared/highlight.directive.ts
@@ -0,0 +1,18 @@
+import { Directive, ElementRef, HostListener } from '@angular/core';
+
+@Directive({
+  selector: '[appHighlight]',
+})
+export class HighlightDirective {
+  constructor(private element: ElementRef) {
+    this.element.nativeElement.style.border = '1px solid transparent';
+  }
+
+  @HostListener('mouseenter') onMouseEnter() {
+    this.element.nativeElement.style.borderColor = 'grey';
+  }
+
+  @HostListener('mouseleave') onMouseLeave() {
+    this.element.nativeElement.style.borderColor = 'transparent';
+  }
+}
